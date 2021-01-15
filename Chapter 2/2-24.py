d15b4f8bad5e503f49a62eac00db8812d0f10a75
Rafael Teixeira <rodriguesteixeirarafael@gmail.com>
2021-01-15 15:08:44 -0300

    Fixed typos and organized the code a little bit

--- Chapter 2/2-24.py
+++ Chapter 2/2-24.py
@@ -1,21 +1,6 @@
-#This program also solves problem 2.24
-
 #!/usr/bin/env python3
+
 import sys 
-import math
-
-results = int(sys.argv[1])
-#networksecurityandcriptographytheoryandpractice
-message = "ofxupsl tfdvsjuz boe dsjquphsbqiz uifpsz boe qsbdujdf"
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-
-alphabetFrequency = {
-    'e' : 12.0, 't' : 9.10, 'a' : 8.12, 'o' : 7.68, 'i' : 7.31, 'n' : 6.95, 
-    's' : 6.28, 'r' : 6.02, 'h' : 5.92, 'd' : 4.32, 'l' : 3.98, 'u' : 2.88,
-    'c' : 2.71, 'm' : 2.61, 'f' : 2.30, 'y' : 2.11, 'w' : 2.09, 'g' : 2.03, 
-    'p' : 1.82, 'b' : 1.49, 'v' : 1.11, 'k' : 0.69, 'x' : 0.17, 'q' : 0.11,
-    'j' : 0.10, 'z' : 0.07 
-}
 
 def decrypt(message, key):
     plaintext = ""
@@ -29,7 +14,17 @@ def decrypt(message, key):
         plaintext += chr(new_char + 97)
     return plaintext
 
+alphabetFrequency = {
+    'e' : 12.0, 't' : 9.10, 'a' : 8.12, 'o' : 7.68, 'i' : 7.31, 'n' : 6.95, 
+    's' : 6.28, 'r' : 6.02, 'h' : 5.92, 'd' : 4.32, 'l' : 3.98, 'u' : 2.88,
+    'c' : 2.71, 'm' : 2.61, 'f' : 2.30, 'y' : 2.11, 'w' : 2.09, 'g' : 2.03, 
+    'p' : 1.82, 'b' : 1.49, 'v' : 1.11, 'k' : 0.69, 'x' : 0.17, 'q' : 0.11,
+    'j' : 0.10, 'z' : 0.07 
+}
 
+results = int(sys.argv[1])
+message = "ofxupsl tfdvsjuz boe dsjquphsbqiz uifpsz boe qsbdujdf"
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
 letterCount = {c: 0 for c in alphabet} 
 letterFrequency = dict()
 
